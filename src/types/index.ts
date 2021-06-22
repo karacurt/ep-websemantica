653e3b01776eaaefbbe96e57ddcc7247c141aee1
Daniel Lima <daniel-lima@usp.br>
2021-06-22 20:55:16 -0300

    types and routes

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,14 @@
 export interface User {
+  id: string
   name: string
   email: string
   password: string
+}
+export interface Product {
+  id: string
+  name: string
+  price: string
+  category: string
 }
 export interface Prefix {
   id: string
