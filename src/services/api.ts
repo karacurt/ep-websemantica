7b416291083e1bcfe2f83550b957c4c8efa7a00b
Daniel Lima <daniel-lima@usp.br>
2021-06-22 19:58:59 -0300

    generalization create op

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,29 @@
 /// <reference path='../types/rdf.d.ts' />
+import rdf from 'rdf'
 import axios from 'axios'
+import { Prefix } from '../types'
+import { graphToTurtle } from './rdf'
 const api = axios.create({ baseURL: 'http://localhost:7200' })
 const BD_NAME = 'rdfforreal'
 
-export async function create(turtle: string) {
+export async function create(data: {}, dataProperties: {}, prefixes: Prefix[]) {
+
+  const dataParsed = rdf.parse(data) 
+
+  const dataPropertiesParsed = rdf.parse(dataProperties)
+  
+ 
+  const dataGraph = dataParsed.graphify()
+  const dataPropertiesGraph = dataPropertiesParsed.graphify()
+
+  const graphs = [dataGraph, dataPropertiesGraph]
+
+  const turtle = graphToTurtle(graphs, prefixes).join('\n')
+
   const response = await api.post(`/repositories/${BD_NAME}/statements`, turtle, { headers: { 'content-type': 'text/turtle' } })
+
   console.log(response)
+
   return response
 }
 
