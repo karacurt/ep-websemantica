137daefce27406fc8d7b1ff8e96b791af84151c8
Daniel Lima <daniel-lima@usp.br>
2021-06-23 00:20:40 -0300

    others

--- src/services/product.ts
+++ src/services/product.ts
@@ -9,25 +9,6 @@ const ep = rdf.ns('http://epwebsemantica.com/')
 const IRI = rdf.ns(`${CLASS_PREFIX}/`)
 
 export async function createProduct(product: Product) {
-  const data = {
-    '@context': {
-      '@vocab': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-      rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
-    },
-    '@id': IRI(product.id),
-    type: ep('product')
-  }
-
-  const dataProperties = {
-    '@context': {
-      '@vocab': `${CLASS_PREFIX}#`,
-      'user-info': `${CLASS_PREFIX}#`
-    },
-    '@id': IRI(product.id),
-    name: rdfjs.literal(product.name, rdf.xsdns('string')),
-    price: rdfjs.literal(product.price, rdf.xsdns('double')),
-    category: rdfjs.literal(product.category, rdf.xsdns('string'))
-  }
-
-  create(data, dataProperties)
+  product.id = product.name
+  create('product', product)
 }
--- src/services/user.ts
+++ src/services/user.ts
@@ -4,29 +4,9 @@ import { Prefix, User } from '../types'
 import { create } from './api'
 
 const rdfjs = rdf.factory
-const foaf = rdf.ns('http://xmlns.com/foaf/0.1/')
-const ep = rdf.ns('http://epwebsemantica.com/')
+//const ep = rdf.ns('http://epwebsemantica.com/')
 
-export async function createUser(user: User) {
-  const data = {
-    '@context': {
-      '@vocab': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-      rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
-    },
-    '@id': ep(`user/${user.name}`),
-    type: ep('user')
-  }
-
-  const dataProperties = {
-    '@context': {
-      '@vocab': 'http://epwebsemantica.com/user#',
-      'user-info': 'http://epwebsemantica.com/user#'
-    },
-    '@id': ep(`user/${user.name}`),
-    name: rdfjs.literal(user.name, rdf.xsdns('string')),
-    email: rdfjs.literal(user.email, rdf.xsdns('string')),
-    password: rdfjs.literal(user.password, rdf.xsdns('string'))
-  }
-
-  create(data, dataProperties)
+export async function createUser(user: any) {
+  user.id = user.name
+  create('user', user)
 }
