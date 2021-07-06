990aa24eef3cda1850a82a641a5595446516fd46
Daniel Lima <daniel-lima@usp.br>
2021-07-06 10:44:24 -0300

    header test

---
+++ src/components/header/index.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+
+export default class MenuExampleInvertedSecondary extends Component {
+  state = { activeItem: 'home' }
+
+  handleItemClick = (e: any, { name }: any) => {
+    this.setState({ activeItem: name })
+  }
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Segment inverted>
+        <Menu inverted pointing secondary>
+          <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick} />
+          <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+        </Menu>
+      </Segment>
+    )
+  }
+}
